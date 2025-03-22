1d94ef1fda7659ea270bafd5a2d1b7595b275af4
kamal-1270 <147371118+kamal-1270@users.noreply.github.com>
2025-03-22 14:12:19 +0530

    "[Kamal] Add. map Interface find employees logic part using hashmap"

---
+++ collections/Map_Interface/GroupObjects/Employee.java
@@ -0,0 +1,27 @@
+package collections.Map_Interface.GroupObjects;
+
+
+
+public class Employee {
+    private String name;
+    private String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
