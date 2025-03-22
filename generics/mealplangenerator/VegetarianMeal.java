3a3e46e73586d6bcbc0978b5ad18e853e162fdbf
kamal-1270 <147371118+kamal-1270@users.noreply.github.com>
2025-03-22 10:21:12 +0530

    "[Kamal] Add .Personalized Meal Plan Generator clasName VegetarianMeal , method displaymeal  using generics"

---
+++ generics/mealplangenerator/VegetarianMeal.java
@@ -0,0 +1,8 @@
+package mealplangenerator;
+import mealplangenerator.*;
+public class VegetarianMeal implements MealPlan {
+    @Override
+    public void displayMeal() {
+        System.out.println("Vegetarian Meal: Grilled vegetables with quinoa.");
+    }
+}
