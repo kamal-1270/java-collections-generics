b146c631ccf1fb031cd95798dab9550610961453
kamal-1270 <147371118+kamal-1270@users.noreply.github.com>
2025-03-22 10:20:24 +0530

    "[Kamal] Add .Personalized Meal Plan Generator clasName VeganMeal , method displaymeal  using generics"

---
+++ generics/mealplangenerator/VeganMeal.java
@@ -0,0 +1,8 @@
+package mealplangenerator;
+import mealplangenerator.*;
+public class VeganMeal implements MealPlan {
+    @Override
+    public void displayMeal() {
+        System.out.println("Vegan Meal: Chickpea salad with avocado dressing.");
+    }
+}
