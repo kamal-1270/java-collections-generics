5aa056c5bc301dea0eea0d880e0892635b98b307
kamal-1270 <147371118+kamal-1270@users.noreply.github.com>
2025-03-22 10:05:49 +0530

    "[Kamal] Add .Personalized Meal Plan Generator clasName KetoMeal displaymeal method using generics"

---
+++ generics/mealplangenerator/KetoMeal.java
@@ -0,0 +1,8 @@
+package mealplangenerator;
+import mealplangenerator.*;
+public class KetoMeal implements MealPlan {
+    @Override
+    public void displayMeal() {
+        System.out.println("Keto Meal: Grilled salmon with buttered spinach.");
+    }
+}
